04222566b919ccf569a956c8308c2deb555df1e6
Markos Alonso Moscoso Ocampo <51459843+markosAMO@users.noreply.github.com>
2023-12-29 17:22:52 -0500

    endpoints
    add api to the endpoints

--- primera_entrega/app.js
+++ primera_entrega/app.js
@@ -4,8 +4,8 @@ import productRoutes from './src/routes/products.js'
 import cartRoutes from './src/routes/carts.js'
 
 app.use(express.json());
-app.use('/products', productRoutes);
-app.use('/carts', cartRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/carts', cartRoutes);
 
 app.listen(8080, () => console.log("Server listen in port 8080"));
 
